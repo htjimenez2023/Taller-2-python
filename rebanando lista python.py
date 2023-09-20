46aa03f8eac2f1a3d3b3104c1ca72973bdfec291
htjimenez <144179330+htjimenez2023@users.noreply.github.com>
2023-09-20 16:37:10 -0500

    Add files via upload

---
+++ rebanando lista python.py
@@ -0,0 +1,13 @@
+Python 3.11.5 (tags/v3.11.5:cce6ba9, Aug 24 2023, 14:38:34) [MSC v.1936 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> numeros = [3,4,5,6,7,8]
+>>> numeros[0]
+3
+>>> numeros[-1]
+8
+>>> numeros[0:3:1]
+[3, 4, 5]
+>>> numeros[0:4:2]
+[3, 5]
+>>> numeros[1:]
+[4, 5, 6, 7, 8]
