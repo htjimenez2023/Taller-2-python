7cec826b587bf5d3d592f135176fdd9f040b9e97
htjimenez <144179330+htjimenez2023@users.noreply.github.com>
2023-09-20 16:30:22 -0500

    Add files via upload

---
+++ ejemplo listas y funcioenes enpython.py
@@ -0,0 +1,11 @@
+Python 3.11.5 (tags/v3.11.5:cce6ba9, Aug 24 2023, 14:38:34) [MSC v.1936 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> a =['a','e','i','o','u']
+>>> print(a)
+['a', 'e', 'i', 'o', 'u']
+>>> del a[1]
+>>> print(a)
+['a', 'i', 'o', 'u']
+>>> del a[2:4]
+>>> print(a)
+['a', 'i']
